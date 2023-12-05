6705a2067d8268b3aab462e5a268f9a296c06c55
yulia1Volkova <volkova.yulia88@yandex.ru>
2023-12-05 14:43:52 +0300

    sprint14

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -2,8 +2,11 @@ package com.practicum.playlistmaker
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
@@ -13,6 +16,7 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -28,7 +32,13 @@ const val HISTORY_KEY = "key_for_history"
 class SearchActivity : AppCompatActivity(), TrackAdapter.TrackClickListener {
     private companion object {
         const val SEARCH_EDITTEXT = "SEARCH_EDITTEXT"
+        private const val CLICK_DEBOUNCE_DELAY = 1000L
+        private const val SEARCH_DEBOUNCE_DELAY = 2000L
     }
+
+    private var isClickAllowed = true
+    private val searchRunnable = Runnable { request() }
+    private val handler = Handler(Looper.getMainLooper())
 
     private lateinit var editSearchHistory: MutableList<Track>
     private var searchEditTextValue: String = ""
@@ -54,8 +64,9 @@ class SearchActivity : AppCompatActivity(), TrackAdapter.TrackClickListener {
     private lateinit var updateButton: Button
     private lateinit var clearHistoryBtn: Button
     private lateinit var historyTextView: TextView
+    private lateinit var progressBar: ProgressBar
     private lateinit var searchHistory: SearchHistory
-
+    private lateinit var historySharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -72,11 +83,11 @@ class SearchActivity : AppCompatActivity(), TrackAdapter.TrackClickListener {
         updateButton = findViewById(R.id.updateButton)
         clearHistoryBtn = findViewById(R.id.clearHistory)
         historyTextView = findViewById(R.id.historyTextView)
+        progressBar =findViewById(R.id.progressBar)
+        historySharedPreferences = getSharedPreferences(HISTORY_PREFERENCES, MODE_PRIVATE)
+        searchHistory = SearchHistory(historySharedPreferences)
 
-        var historySharedPreferences = getSharedPreferences(HISTORY_PREFERENCES, MODE_PRIVATE)
-        val searchHistory = SearchHistory(historySharedPreferences)
-
-        val searchHistoryList: MutableList<Track> =
+        val searchHistoryList: MutableList<Track> =//searchHistory.read().toMutableList()
             ((searchHistory.read())?.toMutableList() ?: listOf<Track>()).toMutableList()
 
         adapterHistory.tracks = searchHistoryList as ArrayList<Track>
@@ -85,10 +96,11 @@ class SearchActivity : AppCompatActivity(), TrackAdapter.TrackClickListener {
 
         searchEditText.setOnFocusChangeListener { view, hasFocus ->
             historyTextView.visibility =
-                if (hasFocus && searchEditText.text.isEmpty() && searchHistoryList.size!=0) View.VISIBLE else View.GONE
-            clearHistoryBtn.visibility = if (hasFocus && searchEditText.text.isEmpty()&& searchHistoryList.size!=0) View.VISIBLE else View.GONE
+                if (hasFocus && searchEditText.text.isEmpty() && searchHistoryList.size != 0) View.VISIBLE else View.GONE
+            clearHistoryBtn.visibility =
+                if (hasFocus && searchEditText.text.isEmpty() && searchHistoryList.size != 0) View.VISIBLE else View.GONE
             recyclerTrack.visibility =
-                if (hasFocus && searchEditText.text.isEmpty() && searchHistoryList.size!=0) View.VISIBLE else View.GONE
+                if (hasFocus && searchEditText.text.isEmpty() && searchHistoryList.size != 0) View.VISIBLE else View.GONE
         }
 
         imageBack.setOnClickListener {
@@ -97,18 +109,18 @@ class SearchActivity : AppCompatActivity(), TrackAdapter.TrackClickListener {
 
         clearButton.setOnClickListener {
             searchEditText.setText("")
-            val searchHistoryList: MutableList<Track> =//searchHistory.read()!!.toMutableList()
-                ((searchHistory.read())?.toMutableList() ?: listOf<Track>()).toMutableList()
-            if (searchHistoryList.size == 0){
-                clearHistoryBtn.visibility= View.GONE
-                historyTextView.visibility= View.GONE
-            } else{
+            val searchHistoryList: MutableList<Track> = searchHistory.read().toMutableList()
+            //((searchHistory.read())?.toMutableList() ?: listOf<Track>()).toMutableList()
+            if (searchHistoryList.size == 0) {
+                clearHistoryBtn.visibility = View.GONE
+                historyTextView.visibility = View.GONE
+            } else {
                 adapterHistory.tracks = searchHistoryList as ArrayList<Track>
                 recyclerTrack.adapter = adapterHistory
                 adapterHistory.notifyDataSetChanged()
-                clearHistoryBtn.visibility= View.VISIBLE
-                historyTextView.visibility= View.VISIBLE
-                recyclerTrack.visibility= View.VISIBLE
+                clearHistoryBtn.visibility = View.VISIBLE
+                historyTextView.visibility = View.VISIBLE
+                recyclerTrack.visibility = View.VISIBLE
             }
 
             val inputMethodManager =
@@ -159,6 +171,7 @@ class SearchActivity : AppCompatActivity(), TrackAdapter.TrackClickListener {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 searchEditTextValue = s.toString()
                 clearButton.visibility = clearButtonVisibility(s)
+                searchDebounce()
 
                 if (searchEditText.hasFocus() && s?.isEmpty() == true && searchHistoryList.size != 0) {
                     historyTextView.visibility = View.VISIBLE
@@ -177,6 +190,20 @@ class SearchActivity : AppCompatActivity(), TrackAdapter.TrackClickListener {
             }
         }
         searchEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+    private fun searchDebounce() {
+        handler.removeCallbacks(searchRunnable)
+        handler.postDelayed(searchRunnable, SEARCH_DEBOUNCE_DELAY)
+    }
+
+    private fun clickDebounce(): Boolean {
+        val current = isClickAllowed
+        if (isClickAllowed) {
+            isClickAllowed = false
+            handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_DELAY)
+        }
+        return current
     }
 
 
@@ -202,12 +229,14 @@ class SearchActivity : AppCompatActivity(), TrackAdapter.TrackClickListener {
 
     private fun request() {
         if (searchEditText.text.isNotEmpty()) {
+            progressBar.visibility = View.VISIBLE
             ITunesService.search(searchEditText.text.toString())
                 .enqueue(object : Callback<TrackResponse> {
                     override fun onResponse(
                         call: Call<TrackResponse>,
                         response: Response<TrackResponse>
                     ) {
+                        progressBar.visibility = View.GONE
                         if (response.code() == 200) {
                             tracks.clear()
                             nothingFindImage.visibility = View.GONE
@@ -219,7 +248,7 @@ class SearchActivity : AppCompatActivity(), TrackAdapter.TrackClickListener {
                                 historyTextView.visibility = View.GONE
 
                                 tracks.addAll(response.body()?.results!!)
-                                adapter.tracks=tracks
+                                adapter.tracks = tracks
                                 adapter.notifyDataSetChanged()
                             }
                             if (tracks.isEmpty()) {
@@ -248,29 +277,37 @@ class SearchActivity : AppCompatActivity(), TrackAdapter.TrackClickListener {
             tracks.clear()
             adapter.notifyDataSetChanged()
             placeholderMessage.text = text
-            if (image)
+            if (image){
                 nothingFindImage.visibility = View.VISIBLE
+            placeholderImage.visibility = View.GONE
+            noInternetMessage.visibility = View.GONE
+            updateButton.visibility = View.GONE
+                progressBar.visibility = View.GONE
+            }
             else {
+                nothingFindImage.visibility = View.GONE
                 placeholderImage.visibility = View.VISIBLE
                 noInternetMessage.visibility = View.VISIBLE
                 updateButton.visibility = View.VISIBLE
+                progressBar.visibility = View.GONE
             }
         }
     }
 
-    
+
     override fun onTrackClick(track: Track) {
-        var historySharedPreferences = getSharedPreferences(HISTORY_PREFERENCES, MODE_PRIVATE)
+        // var historySharedPreferences = getSharedPreferences(HISTORY_PREFERENCES, MODE_PRIVATE)
+        if (clickDebounce()) {
         searchHistory = SearchHistory(historySharedPreferences)
-        val searchHistoryList: MutableList<Track> =//searchHistory.read()!!.toMutableList()
-            ((searchHistory.read())?.toMutableList() ?: listOf<Track>()).toMutableList()
+        val searchHistoryList: MutableList<Track> = searchHistory.read().toMutableList()
+        //((searchHistory.read())?.toMutableList() ?: listOf<Track>()).toMutableList()
         editSearchHistory = searchHistory.editList(searchHistoryList, track)
         adapterHistory.tracks = editSearchHistory as ArrayList<Track>
         adapterHistory.notifyDataSetChanged()
-        val playerIntent = Intent(this, Audioplayer::class.java)
+
+        val playerIntent = Intent(this, AudioPlayerActivity::class.java)
         playerIntent.putExtra("track", track)
         startActivity(playerIntent)
-
-
     }
+}
 }
--- app/src/main/java/com/practicum/playlistmaker/SearchHistory.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchHistory.kt
@@ -3,15 +3,10 @@ package com.practicum.playlistmaker
 import android.content.SharedPreferences
 import com.google.gson.Gson
 
-class SearchHistory(val sharedPreferences: SharedPreferences) {
+class SearchHistory(private val sharedPreferences: SharedPreferences) {
 
-//    fun read(): Array<Track> {
-//        val json = sharedPreferences.getString(HISTORY_KEY, "")
-//        return Gson().fromJson(json, Array<Track>::class.java)
-//    }
-
-    fun read(): Array<Track>? {
-        val json = sharedPreferences.getString(HISTORY_KEY, null) ?: return null
+    fun read(): Array<Track> {
+        val json = sharedPreferences.getString(HISTORY_KEY, null) ?: return emptyArray()
         return Gson().fromJson(json, Array<Track>::class.java)
     }
 
@@ -24,14 +19,14 @@ class SearchHistory(val sharedPreferences: SharedPreferences) {
 
     fun editList(searchHistoryList: MutableList<Track>, track: Track): MutableList<Track> {
         val index = searchHistoryList.indexOfFirst { it.trackId == track.trackId }
-                if (index != -1) {
-                    searchHistoryList.removeAt(index)
-                    searchHistoryList.add(0,track)
-                } else if (searchHistoryList.size<10){
-                    searchHistoryList.add(0,track)
-                } else{
-                    searchHistoryList.removeAt(9)
-                }
+        if (index != -1) {
+            searchHistoryList.removeAt(index)
+            searchHistoryList.add(0, track)
+        } else if (searchHistoryList.size < 10) {
+            searchHistoryList.add(0, track)
+        } else {
+            searchHistoryList.removeAt(9)
+        }
         write(searchHistoryList)
         return searchHistoryList
     }
