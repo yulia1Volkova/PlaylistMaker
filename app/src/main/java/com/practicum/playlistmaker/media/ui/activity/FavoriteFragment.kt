755497d0436751a251a8901b8b7f4aa985cbfc40
yulia1Volkova <volkova.yulia88@yandex.ru>
2024-02-09 19:40:26 +0300

    починили цвет индикатора TabLayout в темной теме, + ViewModels

--- app/src/main/java/com/practicum/playlistmaker/media/ui/activity/FavoriteFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/media/ui/activity/FavoriteFragment.kt
@@ -5,14 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
 import com.practicum.playlistmaker.databinding.FragmentFavoriteBinding
+import com.practicum.playlistmaker.media.ui.view_model.FavoriteFragmentViewModel
+import com.practicum.playlistmaker.media.ui.view_model.PlaylistsFragmentViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
 
 class FavoriteFragment: Fragment() {
 
     companion object{
         fun newInstance()=FavoriteFragment()
     }
-
+    private val viewModel by viewModel<FavoriteFragmentViewModel>()
     private var _binding: FragmentFavoriteBinding?=null
     private val  binding get() = _binding
 
--- app/src/main/java/com/practicum/playlistmaker/media/ui/activity/PlaylistsFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/media/ui/activity/PlaylistsFragment.kt
@@ -7,11 +7,17 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.practicum.playlistmaker.databinding.FragmentFavoriteBinding
 import com.practicum.playlistmaker.databinding.FragmentPlaylistsBinding
+import com.practicum.playlistmaker.media.ui.view_model.FavoriteFragmentViewModel
+import com.practicum.playlistmaker.media.ui.view_model.PlaylistsFragmentViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
 
 class PlaylistsFragment : Fragment() {
     companion object{
         fun newInstance()=PlaylistsFragment()
     }
+
+    private val viewModel by viewModel<PlaylistsFragmentViewModel>()
     private var _binding: FragmentPlaylistsBinding? = null
     private val binding get() = _binding
 
